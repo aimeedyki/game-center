224d5981ed3af1c52ef482e85271df78207f5bfa
aimeedyki <akuwudike.a@gmail.com>
2023-03-04 16:51:37 -0500

    add app setup (#1)

---
+++ src/views/TicTacToe.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tictactoe-board">
+    <div v-for="(n, i) in 3">
+      <div v-for="(n, j) in 3">
+        <cell :value="board[i][j]"></cell>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+  export default {
+    data() { return {
+      board: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', '']
+        ]
+    } },
+  }
+</script>
+<style>
+  .tictactoe-board {
+    display: flex;
+    flex-wrap: wrap;
+    width: 204px;
+    height: 204px;
+  }
+</style>
